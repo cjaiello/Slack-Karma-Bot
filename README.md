59f00d6e6f6749362f3e3ecbd39aa75fb01a4712
Christina <christinajaiello@gmail.com>
2017-09-27 20:12:23 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lumbergh Slack Bot
+A very simple slack bot user.
+
+![screen](screen.png)
---
+++ karmabot.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, Response, jsonify
+import re
+from flask.ext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/karma_database'
+db = SQLAlchemy(app)
+
+@app.route('/karma', methods=['POST'])
+def karma():
+    text = request.form.get('text', '')
+    if '++' in text:
+        # Get username from message
+        # https://pythex.org/
+        matchObj = re.match( r'[\s]?([a-zA-Z0-9]*)\+\+*', text, re.M|re.I)
+
+        # Look for user in database
+
+            # If user is in database, get user's karma from database
+            # Else return
+        # Add 1 to karma
+        # Update user's karma in database
+        # Return karma
+        return jsonify(text=link)
+    return Response(), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
