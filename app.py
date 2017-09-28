948944f6d8561fd6be263cf34799ce5dcc17e303
Christina <christinajaiello@gmail.com>
2017-09-28 19:45:27 -0400

    Adding a real authentication token

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def karma():
         if user_info.get('ok'):
             username_match = user_info['user']['name']
         else:
-            print user_info
+            print(user_info)
     else:
         # Person wasn't tagged, so we have the actual name
         username_match_group = re.search( r'[\W+]?([\w\d_]+)[\s]?(\+\+|--).?', text, re.M|re.I)
