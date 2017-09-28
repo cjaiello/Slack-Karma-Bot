fe275988e07b325b7f153fec38727ec0acf940d6
Christina <christinajaiello@gmail.com>
2017-09-28 19:17:39 -0400

    How did you break?

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ def karma():
         if user_info.get('ok'):
             username_match = user_info['user']['name']
             print(username_match)
+        else:
+            print user_info
     else:
         # Person wasn't tagged, so we have the actual name
         username_match_group = re.search( r'[\W+]?([\w\d_]+)[\s]?(\+\+|--).?', text, re.M|re.I)
