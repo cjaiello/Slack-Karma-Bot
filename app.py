92512b2db423c8ea614f11e52565c848617cb2ba
Christina <christinajaiello@gmail.com>
2017-09-27 21:43:05 -0400

    Potentially got karma working... first test

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, request, Response, jsonify
 import re
-#from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/karma_database'
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql-clean-34734'
+db = SQLAlchemy(app)
+
+# Create our database model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    karma = db.Column(db.Integer)
+
+    def __init__(self, username, karma):
+        self.username = username
+        self.karma = karma
+
+    def __repr__(self):
+        return '<Username %r>' % self.username
 
 @app.route('/')
 def homepage():
@@ -13,20 +27,30 @@ def homepage():
 
 @app.route('/karma', methods=['POST'])
 def karma():
+    karma_recipient = ""
+    karma_number = 1
     text = request.form.get('text', '')
     if '++' in text:
         # Get username from message
         # https://pythex.org/
-        matchObj = re.match( r'[\s]?([a-zA-Z0-9]*)\+\+*', text, re.M|re.I)
+        matchObj = re.match( r'\+\+[\s]?([a-z A-Z 0-9]+))', text, re.M|re.I)
 
         # Look for user in database
-
+        if not db.session.query(User).filter(User.username == username).count():
+            # User isn't in database.
+            # Start them off with 1 karma point
+            user = User(username, 1)
+            db.session.add(user)
+            db.session.commit()
+        else:
             # If user is in database, get user's karma from database
-            # Else return
-        # Add 1 to karma
-        # Update user's karma in database
+            user = User.query.filter_by(User.username == username).first()
+            user.karma = user.karma + 1
+            db.session.commit()
+            karma_number = user.karma
+            karma_recipient = user.username
         # Return karma
-        return jsonify(text="test")
+        return jsonify(text=karma_recipient + "'s karma is now " + karma_number)
     return Response(), 200
 
 if __name__ == '__main__':
