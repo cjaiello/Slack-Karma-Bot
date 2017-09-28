3f8649212397818dbcbb8f657fadeadb6b2a30c0
Christina <christinajaiello@gmail.com>
2017-09-27 22:00:00 -0400

    I should probably import os before using it, huh

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response, jsonify
 import re
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
