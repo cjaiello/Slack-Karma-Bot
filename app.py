e695a6188ab04d38321260e1dbc9a26983035ee6
Christina Aiello <caiello@datto.com>
2020-09-21 12:08:02 -0400

    Going back to old project now that we are verified

--- app.py
+++ app.py
@@ -34,52 +34,47 @@ def homepage():
 
 @app.route('/karma', methods=['GET', 'POST'])
 def karma():
-    print(request)
-    print(request.json)
-    print(request.json['challenge'])
-    return request.json['challenge']
-
-    # slack_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
-    # users_total_karma = 0
-    # username_match = ''
+    slack_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
+    users_total_karma = 0
+    username_match = ''
 
     # Get username from message
     # https://pythex.org/
-    # if '<@' in text:
-    #     # Person was tagged and we actually received an ID
-    #     print("User ID of person we want to give karma to:" + text)
-    #     username_match_group = re.search( r'<@([\w\d_]+)>[\s+]?(\+\+|--).?', text, re.M|re.I)
-    #     user_id = username_match_group.group(1)
-    #     user_info = slack_client.api_call("users.info", user=user_id)
-    #     if user_info.get('ok'):
-    #         username_match = user_info['user']['name']
-    #     else:
-    #         print(user_info)
-    #         return jsonify("Error in giving karma. Please contact caiello@vistaprint.com")
-    # else:
-    #     # Person wasn't tagged, so we have the actual name
-    #     username_match_group = re.search( r'[\W+]?([\w\d_]+)[\s]?(\+\+|--).?', text, re.M|re.I)
-    #     username_match = username_match_group.group(1)
+    if '<@' in text:
+        # Person was tagged and we actually received an ID
+        print("User ID of person we want to give karma to:" + text)
+        username_match_group = re.search( r'<@([\w\d_]+)>[\s+]?(\+\+|--).?', text, re.M|re.I)
+        user_id = username_match_group.group(1)
+        user_info = slack_client.api_call("users.info", user=user_id)
+        if user_info.get('ok'):
+            username_match = user_info['user']['name']
+        else:
+            print(user_info)
+            return jsonify("Error in giving karma. Please contact caiello@vistaprint.com")
+    else:
+        # Person wasn't tagged, so we have the actual name
+        username_match_group = re.search( r'[\W+]?([\w\d_]+)[\s]?(\+\+|--).?', text, re.M|re.I)
+        username_match = username_match_group.group(1)
 
-    # # Determine karma amount based on + or -
-    # karma_given = (text.count('+')-1) if ('+' in text) else (-1 * (text.count('-')-1))
+    # Determine karma amount based on + or -
+    karma_given = (text.count('+')-1) if ('+' in text) else (-1 * (text.count('-')-1))
 
-    # # Look for user in database
-    # if not db.session.query(User).filter(User.username == username_match).count():
-    #     # User isn't in database. Create our user object
-    #     user = User(username_match, karma_given)
-    #     # Add them to the database
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     users_total_karma = karma_given
-    # else:
-    #     # If user is in database, get user's karma from database
-    #     user = User.query.filter_by(username = username_match).first()
-    #     user.karma = user.karma + karma_given
-    #     db.session.commit()
-    #     users_total_karma = user.karma
-    # # Return karma
-    # return jsonify(text=username_match + "'s karma is now " + str(users_total_karma))
+    # Look for user in database
+    if not db.session.query(User).filter(User.username == username_match).count():
+        # User isn't in database. Create our user object
+        user = User(username_match, karma_given)
+        # Add them to the database
+        db.session.add(user)
+        db.session.commit()
+        users_total_karma = karma_given
+    else:
+        # If user is in database, get user's karma from database
+        user = User.query.filter_by(username = username_match).first()
+        user.karma = user.karma + karma_given
+        db.session.commit()
+        users_total_karma = user.karma
+    # Return karma
+    return jsonify(text=username_match + "'s karma is now " + str(users_total_karma))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
